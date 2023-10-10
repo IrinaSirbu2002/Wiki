4318c4e16c067b7a447eb1f791cd790b7108f961
Sirbu Irina <irinasirbu0326@gmail.com>
2023-10-11 00:47:51 +0300

    directories

--- wiki/settings.py
+++ wiki/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'wiki.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["encyclopedia" / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = ["encyclopedia" / "staticfiles"]
 
 WHITENOISE_KEEP_ONLY_HASHED_FILES = True
 
