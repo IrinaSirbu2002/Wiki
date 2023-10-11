acc2e73f8d3823521d45b264beee862ee2eccb68
Sirbu Irina <irinasirbu0326@gmail.com>
2023-10-11 13:38:15 +0300

    still settings

--- wiki/settings.py
+++ wiki/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,7 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-django_heroku.settings(locals())
 
 
 # Default primary key field type
